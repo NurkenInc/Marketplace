02bb3b17a0efaf7ff6ccb1d4d152641909591a19
unknown <nurkenkenes7@gmail.com>
2023-04-17 22:13:14 +0600

    Added navbar to home page

---
+++ client/src/pages/Home.tsx
@@ -0,0 +1,13 @@
+import { Container } from '@chakra-ui/react'
+
+import { Navbar } from '../components'
+
+const Home = () => {
+  return (
+    <Container maxW='container.xl'>
+      <Navbar />
+    </Container>
+  )
+}
+
+export default Home
