9a656e92e50fafeffc4e236a06e141bff92ede1a
unknown <nurkenkenes7@gmail.com>
2023-04-21 23:51:09 +0600

    added clerk provider with routes

--- client/src/App/App.tsx
+++ client/src/App/App.tsx
@@ -1,10 +1,64 @@
+import React from 'react'
+import { 
+  ClerkProvider,
+  SignIn,
+  SignUp
+} from '@clerk/clerk-react'
+import { 
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useNavigate,
+} from 'react-router-dom'
 
 import { Home } from '../pages'
+
 import './App.css'
 
-function App() {
+const clerkPubKey = import.meta.env.VITE_REACT_APP_CLERK_PUBLISHABLE_KEY
+
+const ClerkProviderWithRoutes = () => {
+  const navigate = useNavigate()
+
   return (
-    <Home />
+    <ClerkProvider 
+      publishableKey={clerkPubKey}
+      navigate={(to) => navigate(to)}
+    >
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route 
+          path='/sign-in/*'
+          element={
+            <SignIn 
+              routing='path' 
+              path='/sign-in' 
+              redirectUrl='/'
+              signUpUrl='/sign-up'
+            />
+          }
+        />
+        <Route 
+          path='/sign-up/*'
+          element={
+            <SignUp 
+              routing='path' 
+              path='/sign-up' 
+              redirectUrl={'/'}
+              signInUrl='/sign-in' 
+            />
+          }
+        />
+      </Routes>
+    </ClerkProvider>
+  )
+}
+
+const App = () => {
+  return (
+    <Router>
+      <ClerkProviderWithRoutes />
+    </Router>
   )
 }
 
