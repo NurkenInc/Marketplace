2ed6131b373b2bd2ff45d5363284cceec046ea67
unknown <nurkenkenes7@gmail.com>
2023-04-21 23:50:48 +0600

    cleared main file from auth0 provider

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,20 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App/App'
-import { Auth0Provider } from '@auth0/auth0-react'
 
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <Auth0Provider
-      domain={import.meta.env.VITE_AUTH0_DOMAIN}
-      clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
-      authorizationParams={{
-        redirect_uri: import.meta.env.VITE_BASE_URL
-      }}
-    >
-      <App />
-    </Auth0Provider>
+    <App />
   </React.StrictMode>,
 )
