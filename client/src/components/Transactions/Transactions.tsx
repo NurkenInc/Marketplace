58c8afc7a3b6c35c66847d50a4522f8174d2fb27
unknown <nurkenkenes7@gmail.com>
2023-04-24 23:05:36 +0600

    added live transactions component

---
+++ client/src/components/Transactions/Transactions.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Transactions = () => {
+  return (
+    <div className='transactions p-4 bg-gray-100 transition-colors duration-500'>
+      <h4 className='font-bold italic'>Live transactions</h4>
+    </div>
+  )
+}
+
+export default Transactions
