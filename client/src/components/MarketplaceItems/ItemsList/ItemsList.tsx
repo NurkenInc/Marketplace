8a3d78688fad8c5dbcb50ed05bdf29f27400ba90
unknown <nurkenkenes7@gmail.com>
2023-04-24 23:03:58 +0600

    added custom styles to items list table

--- client/src/components/MarketplaceItems/ItemsList/ItemsList.tsx
+++ client/src/components/MarketplaceItems/ItemsList/ItemsList.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const testData = [
   {
@@ -6,6 +7,7 @@ const testData = [
       img: '/logo-variant2.png',
       name: 'YO'
     },
+    id: 'id',
     itemName: 'Test1',
     itemPrice: '10',
     currency: '$'
@@ -15,6 +17,7 @@ const testData = [
       img: '/logo-variant2.png',
       name: 'LOL'
     },
+    id: 'id2',
     itemName: 'Test8',
     itemPrice: '10',
     currency: '$'
@@ -24,47 +27,98 @@ const testData = [
       img: '/logo-variant1.png',
       name: 'hahaha'
     },
+    id: 'id3',
     itemName: 'Test2mb',
     itemPrice: '1220',
     currency: '$'
-  }
+  },
+  {
+    user: {
+      img: '/logo-variant1.png',
+      name: 'hahaha'
+    },
+    id: 'id3',
+    itemName: 'Test2mb',
+    itemPrice: '1220',
+    currency: '$'
+  },
+  {
+    user: {
+      img: '/logo-variant1.png',
+      name: 'hahaha'
+    },
+    id: 'id3',
+    itemName: 'Test2mb',
+    itemPrice: '1220',
+    currency: '$'
+  },
+  {
+    user: {
+      img: '/logo-variant1.png',
+      name: 'hahaha'
+    },
+    id: 'id3',
+    itemName: 'Test2mb',
+    itemPrice: '1220',
+    currency: '$'
+  },
+  {
+    user: {
+      img: '/logo-variant1.png',
+      name: 'hahaha'
+    },
+    id: 'id3',
+    itemName: 'Test2mb',
+    itemPrice: '1220',
+    currency: '$'
+  },
 ]
 
 const ItemsList = () => {
-  
+  const navigate = useNavigate()
+
+  const handleDetails = () => {
+    navigate('item/details/:id')
+  }
+
   return (
-    <div className='overflow-x-auto w-full py-5'>
+    <div>
       <h1 className='py-2 font-bold'>Current offers</h1>
-      <table className='table w-full'>
-        <thead className=''>
-          <th>User</th>
-          <th>Item Name</th>
-          <th>Item Price</th>
-          <th></th>
-        </thead>
-        <tbody>
-          {
-            testData.map((item, index) => (
-              <tr>
-                <td className='flex items-center gap-2'>
-                  <img 
-                    className='w-[35px] rounded-[30px] bg-purple-400'
-                    src={item?.user?.img}
-                  />
-                  {item?.user?.name}
-                </td>
-                <td>{item?.itemName}</td>
-                <td>{item?.itemPrice + item?.currency}</td>
-                <td>
-                  <button className='rounded-[25px] bg-purple-400'>
-                    Details
-                  </button>
-                </td>
-              </tr>
-            ))
-          }
-        </tbody>
-      </table>
+      <div className='overflow-x-auto w-full py-5 overflow-y-scroll h-[260px]'>
+        <table className='custom-table w-full'>
+          <thead className='bg-gray-200 font-mono text-left'>
+            <th className='py-4 px-8'>User</th>
+            <th>Item Name</th>
+            <th>Item Price</th>
+            <th></th>
+          </thead>
+          <tbody>
+            {
+              testData.map((item, index) => (
+                <tr className='bg-gradient-to-r from-violet-500 to-fuchsia-500 transition-colors duration-500'>
+                  <td className='flex items-center gap-2 py-4 px-8'>
+                    <img
+                      className='w-[35px] rounded-[30px] bg-purple-400'
+                      src={item?.user?.img}
+                    />
+                    {item?.user?.name}
+                  </td>
+                  <td>{item?.itemName}</td>
+                  <td>{item?.itemPrice + item?.currency}</td>
+                  <td>
+                    <button 
+                      className='rounded-[25px] bg-purple-400 transition-colors duration-400' 
+                      onClick={handleDetails}
+                    >
+                      Details
+                    </button>
+                  </td>
+                </tr>
+              ))
+            }
+          </tbody>
+        </table>
+      </div>
     </div>
   )
 }
