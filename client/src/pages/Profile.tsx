3ee8daa7aa4584dcd39844396298da06c5f54902
unknown <nurkenkenes7@gmail.com>
2023-04-24 23:07:04 +0600

    added user profile page

--- client/src/pages/Profile.tsx
+++ client/src/pages/Profile.tsx
@@ -1,5 +1,10 @@
+import React from 'react'
+
 const Profile = () => {
-    
+  return (
+    <div>User profile</div>
+  )
 }
 
+export default Profile
 // items list where you can add items to your profile
