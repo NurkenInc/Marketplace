44662f07c2e3382eb611b06489cc3d96873cbc89
unknown <nurkenkenes7@gmail.com>
2023-04-24 23:03:23 +0600

    exported transactions component

--- client/src/components/index.ts
+++ client/src/components/index.ts
@@ -2,10 +2,12 @@ import Navbar from './Navbar/Navbar'
 import Logotype from './Logotype/Logotype'
 import ToggleModeButton from './ToggleModeButton/ToggleModeButton'
 import ItemsList from './MarketplaceItems/ItemsList/ItemsList'
+import Transactions from './Transactions/Transactions'
 
 export {
   Navbar,
   Logotype,
   ToggleModeButton,
   ItemsList,
+  Transactions,
 }
