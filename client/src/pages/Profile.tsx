e0210fb1bfa1af7d8cb4fa6243441a7ee2c70260
unknown <nurkenkenes7@gmail.com>
2023-04-25 22:10:17 +0600

    added clerk user btn and items section to profile page

--- client/src/pages/Profile.tsx
+++ client/src/pages/Profile.tsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import { UserButton, useUser } from '@clerk/clerk-react'
+import { CiUser } from 'react-icons/ci'
 
 const Profile = () => {
+  const { user } = useUser()
+
   return (
-    <div>User profile</div>
+    <div>
+      <div className={`flex justify-between`}>
+        <div className='flex items-center gap-2 p-4'>
+          {/* <img src={user?.profileImageUrl} className='rounded-[50%] w-[3rem]' /> */}
+          <UserButton />
+          <h1 className='text-[30px]'>
+            {user?.firstName}
+          </h1>
+        </div>
+        <div className='p-4'>
+        </div>
+      </div>
+      <div>
+        <h2>Items</h2>
+        <button>Add item</button>
+      </div>
+    </div>
   )
 }
 
