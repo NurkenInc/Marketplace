c3b7150a5b237cad693e4e2a0916457977132cbb
NurkenInc <nurkenkenes7@gmail.com>
2023-05-01 23:13:44 +0600

    empty commit

--- client/src/components/Transactions/Transactions.tsx
+++ client/src/components/Transactions/Transactions.tsx
@@ -5,9 +5,6 @@ const Transactions = () => {
     <div className='transactions p-4 bg-gray-100 transition-colors duration-500'>
       <h4 className='font-bold italic'>Live transactions</h4>
       <ul>
-        {
-          <li></li>
-        }
       </ul>
     </div>
   )
