058a937714f01b985ad2415b9509545210d3619d
NurkenInc <@nurkenkenes7@gmail.com>
2023-05-01 23:05:12 +0600

    added  list of transactions(test)

--- client/src/components/Transactions/Transactions.tsx
+++ client/src/components/Transactions/Transactions.tsx
@@ -4,6 +4,11 @@ const Transactions = () => {
   return (
     <div className='transactions p-4 bg-gray-100 transition-colors duration-500'>
       <h4 className='font-bold italic'>Live transactions</h4>
+      <ul>
+        {
+          <li></li>
+        }
+      </ul>
     </div>
   )
 }
