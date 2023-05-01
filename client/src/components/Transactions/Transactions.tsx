7f8cc95a5478931f70f0e96f28e97387fc909e38
NurkenInc <nurkenkenes7@gmail.com>
2023-05-01 23:17:52 +0600

    removed extra list

--- client/src/components/Transactions/Transactions.tsx
+++ client/src/components/Transactions/Transactions.tsx
@@ -4,9 +4,6 @@ const Transactions = () => {
   return (
     <div className='transactions p-4 bg-gray-100 transition-colors duration-500'>
       <h4 className='font-bold italic'>Live transactions</h4>
-      <ul>
-        
-      </ul>
     </div>
   )
 }
